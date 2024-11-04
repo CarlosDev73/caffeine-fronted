cc071bbea40ae863fa763324ededa99845f6268a
Utilit1s <fran@puntocero.agency>
2024-11-04 17:19:03 -0400

    Agregar rutas al menu

--- components/MainPanel.jsx
+++ components/MainPanel.jsx
@@ -1,39 +1,38 @@
-import { Pressable, StyleSheet, Text, View, Image } from 'react-native'
-import React from 'react'
-import { theme } from '../constants/theme'
-import { heightPercentage, widthPercentage } from '../helpers/common'
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { theme } from '../constants/theme';
+import { heightPercentage, widthPercentage } from '../helpers/common';
 import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
 import Fontisto from '@expo/vector-icons/Fontisto';
 import Feather from '@expo/vector-icons/Feather';
+import { useRouter } from 'expo-router';
 
-const MainPanel = ({
-  onPress=()=>{},
-}) => {
+const MainPanel = () => {
+  const router = useRouter();
 
   return (
     <View style={styles.container}>
-      <Pressable onPress={onPress} style={[styles.button]}>
-      <SimpleLineIcons name="home" size={23} color="black" />
-        <Text style={[styles.text]}>Home</Text>
+      <Pressable onPress={() => router.push('/feed')} style={styles.button}>
+        <SimpleLineIcons name="home" size={23} color="black" />
+        <Text style={styles.text}>Home</Text>
       </Pressable>
-      <Pressable onPress={onPress} style={[styles.button]}>
+      <Pressable onPress={() => router.push('/searchProfile')} style={styles.button}>
         <Fontisto name="search" size={23} color="black" />
-        <Text style={[styles.text]}>Buscar</Text>
+        <Text style={styles.text}>Buscar</Text>
       </Pressable>
-      <Pressable onPress={onPress} style={[styles.button]}>
+      <Pressable onPress={() => router.push('/favorites')} style={styles.button}>
         <Feather name="heart" size={23} color="black" />
-        <Text style={[styles.text]}>Favoritos</Text>
+        <Text style={styles.text}>Favoritos</Text>
       </Pressable>
-      <Pressable onPress={onPress} style={[styles.button]}>
+      <Pressable onPress={() => router.push('/profile')} style={styles.button}>
         <Feather name="user" size={24} color="black" />
-        <Text style={[styles.text]}>Perfil</Text>
+        <Text style={styles.text}>Perfil</Text>
       </Pressable>
     </View>
-    
-  )
-}
+  );
+};
 
-export default MainPanel
+export default MainPanel;
 
 const styles = StyleSheet.create({
   container: {
@@ -42,25 +41,22 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-evenly',
     width: widthPercentage(100),
-    paddingBottom: 10
+    paddingBottom: 10,
   },
-  icon: {
-
-  },
-  button:{
-    height: heightPercentage(7,7),
+  button: {
+    height: heightPercentage(7, 7),
     width: widthPercentage(20),
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 8
+    marginTop: 8,
   },
-  text:{
+  text: {
     fontSize: heightPercentage(2),
     fontWeight: theme.fonts.bold,
     marginTop: 4,
-    color: 'gray'
+    color: 'gray',
   },
   active: {
-    color: 'black'
-  }
-})
+    color: 'black',
+  },
+});
